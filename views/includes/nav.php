355e0b3b626024f1a8e0450e115558bf5c0aeb60
Louie <louieabobo12345@gmail.com>
2024-08-12 15:45:10 +0800

    Update nav.php

--- views/includes/nav.php
+++ views/includes/nav.php
@@ -6,12 +6,12 @@
         </button>
     </div>
     <div class="p-4 pt-5">
-       <div class="mb-5 text-center justify-content-center">
-                <div>
-                    <img src="/assets/smcc-logo.png" alt="SMCC-LOGO" width="50px" height="50px">
-                    <h1 class="h6">SAINT MICHAEL COLLEGE OF CARAGA</h1>
-                </div>
+        <div class="mb-5 text-center justify-content-center">
+            <div>
+                <img src="/assets/smcc-logo.png" alt="SMCC-LOGO" width="50px" height="50px">
+                <h1 class="h6">SAINT MICHAEL COLLEGE OF CARAGA</h1>
             </div>
+        </div>
 
         <ul class="list-unstyled components mb-5">
             <?php if ($_SESSION['role'] != 'student'): ?>
@@ -20,16 +20,16 @@
                 </li>
             <?php endif; ?>
             <li>
-                <a href="/views/profile/profile.php"><i class="fa fa-user"></i> PROFILE</a>
+                <a href="/views/profile/profile.php"><i class="fas fa-user-circle"></i> PROFILE</a>
             </li>
             <?php if ($_SESSION['role'] == 'Admin'): ?>
                 <li>
-                    <a href="/views/accounts/accounts.php"><i class="fa fa-user"></i> USER ACCOUNTS</a>
+                    <a href="/views/accounts/accounts.php"><i class="fas fa-users"></i> USER ACCOUNTS</a>
                 </li>
             <?php endif; ?>
             <li>
                 <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                    <i class="fa fa-calendar-check"></i> SCHEDULES
+                    <i class="fas fa-calendar-alt"></i> SCHEDULES
                 </a>
                 <ul class="collapse list-unstyled" id="homeSubmenu">
                     <li><a href="/views/laboratories/lab1.php">Computer lab 1</a></li>
